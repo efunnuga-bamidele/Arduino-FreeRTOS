2872752b775442348b6404b524e5ee16d184f55b
Bamidele Efunnuga <bamideleefunnuga9@gmail.com>
2022-07-19 06:40:17 +0100

    Serial Print Profiler feedback

--- Profiling_ledBlinking/Profiling_ledBlinking.ino
+++ Profiling_ledBlinking/Profiling_ledBlinking.ino
@@ -4,8 +4,14 @@
 #define YELLOW 7
 #define BLUE   8
 
+typedef int TaskProfiler;
+
+TaskProfiler REDLEDProfiler;
+TaskProfiler BLUELEDProfiler;
+TaskProfiler YELLOWLEDProfiler;
 
 void setup() {
+  Serial.begin(9600);
   xTaskCreate(redLedControllerTask, "RED LED Task", 128, NULL, 1, NULL);
   xTaskCreate(blueLedControllerTask, "BLUE LED Task", 128, NULL, 1, NULL);
   xTaskCreate(yellowLedControllerTask, "YELLOW LED Task", 128, NULL, 1, NULL);
@@ -17,6 +23,9 @@ void redLedControllerTask(void *pvParameters)
   pinMode(RED, OUTPUT);
   while(1)
   {
+    REDLEDProfiler++;
+    Serial.print("RedLEDProfiler : ");
+    Serial.println(REDLEDProfiler);
     digitalWrite(RED, digitalRead(RED)^1);
   }
 }
@@ -26,6 +35,9 @@ void blueLedControllerTask(void *pvParameters)
   pinMode(BLUE, OUTPUT);
   while(1)
   {
+    BLUELEDProfiler++;
+    Serial.print("BlueLEDProfiler : ");
+    Serial.println(BLUELEDProfiler);
     digitalWrite(BLUE, digitalRead(BLUE)^1);
   }
 }
@@ -35,6 +47,9 @@ void yellowLedControllerTask(void *pvParameters)
   pinMode(YELLOW, OUTPUT);
   while(1)
   {
+    YELLOWLEDProfiler++;
+    Serial.print("YellowLEDProfiler : ");
+    Serial.println(YELLOWLEDProfiler);
     digitalWrite(YELLOW, digitalRead(YELLOW)^1);
   }
 }
