0433f2e9e78865c87dfc05611d3b29c1b8a33433
Bamidele Efunnuga <bamideleefunnuga9@gmail.com>
2022-07-19 06:34:48 +0100

    Profiling Led Blinker

---
+++ Profiling_ledBlinking/Profiling_ledBlinking.ino
@@ -0,0 +1,42 @@
+#include <Arduino_FreeRTOS.h>
+
+#define RED    6
+#define YELLOW 7
+#define BLUE   8
+
+
+void setup() {
+  xTaskCreate(redLedControllerTask, "RED LED Task", 128, NULL, 1, NULL);
+  xTaskCreate(blueLedControllerTask, "BLUE LED Task", 128, NULL, 1, NULL);
+  xTaskCreate(yellowLedControllerTask, "YELLOW LED Task", 128, NULL, 1, NULL);
+}
+
+//Task functions
+void redLedControllerTask(void *pvParameters)
+{
+  pinMode(RED, OUTPUT);
+  while(1)
+  {
+    digitalWrite(RED, digitalRead(RED)^1);
+  }
+}
+
+void blueLedControllerTask(void *pvParameters)
+{
+  pinMode(BLUE, OUTPUT);
+  while(1)
+  {
+    digitalWrite(BLUE, digitalRead(BLUE)^1);
+  }
+}
+
+void yellowLedControllerTask(void *pvParameters)
+{
+  pinMode(YELLOW, OUTPUT);
+  while(1)
+  {
+    digitalWrite(YELLOW, digitalRead(YELLOW)^1);
+  }
+}
+
+void loop() {}
